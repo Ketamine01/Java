e648888147a2f67bdac3b7f4b26c17e9d6cbef60
Максим <fedyush90@mail.ru>
2023-05-25 13:28:35 +0300

    вэл дан

---
+++ Homework4/Task4_2_1.java
@@ -0,0 +1,20 @@
+package Homework4;
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Task4_2_1 {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        LinkedList<String> reversedList = reverseLinkedList(list);
+        System.out.println(reversedList); // [c, b, a]
+    }
+
+    public static LinkedList reverseLinkedList(LinkedList list) {
+        Collections.reverse(list);
+        return list;
+    }
+}
